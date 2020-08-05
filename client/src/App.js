9405eb7fa8f9f70c6bf944d3db3e78a12913334b
Jin Lin Cen <jinlincen@JinLins-MacBook-Pro.local>
2020-08-05 17:46:51 -0400

    minor bugs fixed, google analytics updateds

--- client/src/App.js
+++ client/src/App.js
@@ -18,8 +18,7 @@ import Signup from "./components/auth/Signup";
 import Login from "./components/auth/Login";
 import PrivateRoute from './components/private-route/PrivateRoute'; // no '{}' means importing the default part of the file
 import UserAvatar from './components/UserAvatar'; // '' is preferred over ""
-// for google analytics
-import ReactGA from 'react-ga';
+
 
 const styles = {
   title: {
@@ -33,13 +32,8 @@ const styles = {
   }
 }
 
-function initializeReactGA() { // are there any other ways to write this???
-    ReactGA.initialize('UA-174542858-1');
-    ReactGA.pageview('/homepage');
-}
-
-
 class App extends Component {
+
   render() {
     return (
       <Provider store={store}>
